63f07874a46a24703b7a357734e0ba4aafaec399
isaqueM <associa.adv@gmail.com>
2026-03-08 21:38:59 -0300

    fix: adicionar alias do react-native-web para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+        'react-native': 'react-native-web',
       }
     }
   };
